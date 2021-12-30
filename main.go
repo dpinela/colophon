713c2efd8481fd6e7dd3d42fb5af9ef786465b0d
Diogo Pinela <diogoid7400@gmail.com>
2021-12-30 01:37:45 +0000

    Add search and detail options to the list command

--- main.go
+++ main.go
@@ -1,20 +1,73 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
+	"regexp"
+	"sort"
+	"strings"
 
 	"github.com/dpinela/hkmod/internal/modlinks"
 )
 
 func main() {
-	manifests, err := modlinks.Get()
+	if len(os.Args) < 2 {
+		fmt.Printf("usage: %s {list}\n", os.Args[0])
+		os.Exit(2)
+	}
+	subcmd := os.Args[1]
+	var err error
+	switch subcmd {
+	case "list":
+		err = list(os.Args[2:])
+	default:
+		err = fmt.Errorf("unknown subcommand: %q", subcmd)
+	}
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
-		return
 	}
+}
+
+func list(args []string) error {
+	flags := flag.NewFlagSet("list", flag.ExitOnError)
+	var detailed bool
+	var search string
+	flags.BoolVar(&detailed, "d", false, "Display detailed information about mods")
+	flags.StringVar(&search, "s", "", "Search for mods whose name contains `term`")
+	if err := flags.Parse(args); err != nil {
+		return err
+	}
+	manifests, err := modlinks.Get()
+	if err != nil {
+		return err
+	}
+	if search != "" {
+		pattern, err := regexp.Compile("(?i)" + regexp.QuoteMeta(search))
+		if err != nil {
+			return err
+		}
+		filtered := manifests[:0]
+		for _, m := range manifests {
+			if pattern.MatchString(m.Name) {
+				filtered = append(filtered, m)
+			}
+		}
+		manifests = filtered
+	}
+	sort.Slice(manifests, func(i, j int) bool { return manifests[i].Name < manifests[j].Name })
 	for _, m := range manifests {
-		fmt.Printf("%#v\n", m)
+		fmt.Println(m.Name)
+		if detailed {
+			fmt.Println("\tVersion:", m.Version)
+			deps := "none"
+			if len(m.Dependencies) > 0 {
+				deps = strings.Join(m.Dependencies, ", ")
+			}
+			fmt.Println("\tDependencies:", deps)
+			fmt.Printf("\t%s\n\n", strings.ReplaceAll(m.Description, "\n", "\n\t"))
+		}
 	}
+	return nil
 }
