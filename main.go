cbcae9379c86c6f96ec57e489882b4cf40d177ee
Diogo Pinela <diogoid7400@gmail.com>
2022-01-01 02:43:00 +0000

    Implement download command

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"archive/zip"
+	"bytes"
+	"crypto/sha256"
+	"encoding/hex"
 	"flag"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
+	"path/filepath"
 	"regexp"
 	"sort"
 	"strings"
@@ -33,6 +40,10 @@ func main() {
 }
 
 func download(mods []string) error {
+	installdir := os.Getenv("HK15PATH")
+	if installdir == "" {
+		return fmt.Errorf("HK15PATH not defined")
+	}
 	manifests, err := modlinks.Get()
 	if err != nil {
 		return err
@@ -41,10 +52,92 @@ func download(mods []string) error {
 	if err != nil {
 		return err
 	}
-	sort.Slice(downloads, func(i, j int) bool { return downloads[i].Name < downloads[j].Name })
 	for _, dl := range downloads {
 		fmt.Println("Installing", dl.Name)
 		fmt.Println("Downloading", dl.Link.URL)
+		file, err := downloadLink(dl.Link)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		fmt.Println("Extracting", dl.Name)
+		if err := extractMod(file, dl.Name, installdir); err != nil {
+			fmt.Println(err)
+		}
+	}
+	return nil
+}
+
+func downloadLink(link modlinks.Link) (*bytes.Reader, error) {
+	wrap := func(err error) error { return fmt.Errorf("download %s: %w", link.URL, err) }
+	expectedSHA, err := hex.DecodeString(link.SHA256)
+	if err != nil {
+		return nil, wrap(err)
+	}
+	resp, err := http.Get(link.URL)
+	if err != nil {
+		return nil, wrap(err)
+	}
+	defer resp.Body.Close()
+	if !(resp.StatusCode >= 200 && resp.StatusCode < 300) {
+		return nil, fmt.Errorf("download %s: response status was %d", link.URL, resp.StatusCode)
+	}
+	sha := sha256.New()
+	buf, err := io.ReadAll(io.TeeReader(resp.Body, sha))
+	if err != nil {
+		return nil, wrap(err)
+	}
+	if !bytes.Equal(sha.Sum(make([]byte, 0, sha256.Size)), expectedSHA) {
+		return nil, fmt.Errorf("download %s: sha256 does not match manifest", link.URL)
+	}
+	return bytes.NewReader(buf), nil
+}
+
+func extractMod(zipfile *bytes.Reader, name, installdir string) error {
+	wrap := func(err error) error { return fmt.Errorf("extract mod %s: %w", name, err) }
+	archive, err := zip.NewReader(zipfile, zipfile.Size())
+	if err != nil {
+		return wrap(err)
+	}
+	for _, file := range archive.File {
+		// Prevent us from accidentally (or not so accidentally, in case of a malicious input)
+		// from writing outside the destination directory.
+		dest := filepath.Join(installdir, "Mods", name, filepath.Join(string(filepath.Separator), filepath.FromSlash(file.Name)))
+		if err := writeZipFile(dest, file); err != nil {
+			return wrap(err)
+		}
+	}
+	return nil
+}
+
+func writeZipFile(dest string, file *zip.File) error {
+	if err := os.MkdirAll(filepath.Dir(dest), 0750); err != nil {
+		return err
+	}
+	w, err := os.Create(dest)
+	if err != nil {
+		return err
+	}
+	r, err := file.Open()
+	if err != nil {
+		w.Close()
+		return err
+	}
+	_, err = io.Copy(w, r)
+	if err != nil {
+		r.Close()
+		w.Close()
+		return err
+	}
+	if err := r.Close(); err != nil {
+		w.Close()
+		return err
+	}
+	if err := w.Close(); err != nil {
+		return err
+	}
+	if err := os.Chtimes(dest, file.Modified, file.Modified); err != nil {
+		fmt.Println("warning:", err)
 	}
 	return nil
 }
